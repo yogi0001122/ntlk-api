f193e8c4203d6e1cc8b5f24a20c314e32f4c51d3
Kumar <yk185029@ncr.com>
2020-11-19 21:10:10 +0530

    added yogi

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["DEBUG"] = True
 
 @app.route("/")
 def hello():
-    return "Hello Flask, on Azure App Service for Linux"
+    return "Hello Yogi, on Azure App Service for Linux"
 
 
 @app.route('/api/v1/resources/books/all', methods=['GET'])
