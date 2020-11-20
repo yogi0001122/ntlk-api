51c71f3268a7c3d373e120547dee117535920dd3
yogi0001122 <56965623+yogi0001122@users.noreply.github.com>
2020-11-20 16:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["DEBUG"] = True
 
 @app.route("/")
 def hello():
-    return "Hello Yogi Kumar, on Azure App Service for Linux"
+    return "Hello Yogi, on Azure App Service for Linux"
 
 
 
